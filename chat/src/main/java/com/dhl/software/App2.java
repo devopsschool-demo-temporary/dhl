b6cc986eb3ca4686b3f96354384f02f06130aaac
Rajesh Kumar <devops@rajeshkumar.xyz>
2020-07-02 09:22:21 +0530

    adding my changes

---
+++ chat/src/main/java/com/dhl/software/App2.java
@@ -0,0 +1,13 @@
+package com.dhl.software;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
